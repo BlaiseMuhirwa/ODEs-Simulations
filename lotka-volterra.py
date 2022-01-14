67fc5a563259665407695011bbe8fa939cd8400a
Blaise Muhirwa <blaisemunyampirwa@Blaises-MacBook-Pro.local>
2022-01-14 03:39:19 -0600

    add function to commpute growth rates

--- lotka-volterra.py
+++ lotka-volterra.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib as plt
 import scipy
 import pylab 
@@ -19,16 +19,26 @@ import argparse
 """
 
 
+def get_growth_rates(init_cond, alpha, beta, gamma, delta):
+    prey_growth_rate = alpha * init_cond[0] - beta * init_cond[0] * init_cond[1]
+    pred_growth_rate = delta * init_cond[0] * init_cond[1] - gamma * init_cond[1]
+    return np.array([prey_growth_rate, pred_growth_rate])
+
+
 def main(args):
     #retrieve model parameters
     alpha = args.alpha
     beta = args.beta
     gamma = args.gamma
     delta = args.delta
+    initial_condition = np.array([args.init_prey, args.init_pred])
+
 
     print('Model Parameters => alpha = {}, beta = {}, gamma = {}, delta = {}'.\
                                             format(alpha, beta, gamma, delta))
-    
+
+    """ Plotting the evolution of the rabit and wolves populations"""
+
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='simulation of the Lotka-Volterra model in a 2D phase space')
@@ -40,6 +50,10 @@ if __name__=='__main__':
                                 help='delta parameter in the second equaation')
     parser.add_argument('--gamma', type=float, default=0.75,
                                 help='gamma parameter in the second equation')
+    parser.add_argument('--init_prey', type=int, default=10,
+                                help='initial population of the prey(default=10)')
+    parser.add_argument('--init_pred', type=int, default=5,
+                                help='initial population of the predator(default=5)')
 
     args = parser.parse_args()
     main(args)
