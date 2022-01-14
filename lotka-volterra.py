b342ed090bce6af08c986cd16f9de8205243134f
Blaise Muhirwa <blaisemunyampirwa@Blaises-MacBook-Pro.local>
2022-01-14 13:39:29 -0600

    finish model implementation

--- lotka-volterra.py
+++ lotka-volterra.py
@@ -20,6 +20,40 @@ import argparse
 """
 
 
+
+def plot_phase_curve(callable, alpha, beta, gamma, delta, time):
+    values = np.linspace(0.2, 0.9, 4)
+    colors = pylab.cm.autumn_r(np.linspace(0.3, 1., len(values)))
+
+    figure = pylab.figure()
+    for value in values:
+        initial_position = value * np.array([gamma/delta, alpha/beta])
+        solution, _ = integrate.odeint(callable, initial_position,time,
+                                full_output=True)
+        pylab.plot(
+            solution[:,0], solution[:,1], lw=3.5*value
+        )
+    """ compute vector at each point to get the vector field"""
+    x_axis = np.linspace(0, pylab.xlim(xmin=0)[1], 20)
+    y_axis = np.linspace(0, pylab.ylim(ymin=0)[1], 20)
+
+    grid_x, grid_y = np.meshgrid(x_axis, y_axis)
+    growth_rate_x, growth_rate_y = callable([grid_x, grid_y])
+
+    """ plotting the phase curve"""
+    pylab.title('Trajectories and direction fields')
+    pylab.quiver(
+        grid_x, grid_y, growth_rate_x, growth_rate_y, pivot='mid'
+        )
+    pylab.xlabel('Prey Population')
+    pylab.ylabel('Predator Population')
+    pylab.grid()
+    pylab.xlim(0, pylab.xlim(xmin=0)[1])
+    pylab.ylim(0, pylab.ylim(ymin=0)[1])
+    figure.savefig('results/phase_curve.png')
+
+
+
 def main(args):
     """ retrieve model parameters """
     alpha = args.alpha
@@ -34,7 +68,7 @@ def main(args):
 
     def get_growth_rates(initial_condition, time=0):
         prey_growth_rate = alpha * initial_condition[0] - beta * initial_condition[0] * initial_condition[1]
-        pred_growth_rate = delta * beta * initial_condition[0] * initial_condition[1] - gamma * initial_condition[1]
+        pred_growth_rate = delta * initial_condition[0] * initial_condition[1] - gamma * initial_condition[1]
         return np.array([prey_growth_rate, pred_growth_rate])
 
     """ Solve the ODE"""
@@ -42,6 +76,7 @@ def main(args):
     solution, message = integrate.odeint(get_growth_rates, initial_condition,
                                                 time, full_output=True)
 
+    """ Plotting the evolution of the rabit and wolves populations"""
     prey, predator = solution.T
     figure1 = pylab.figure()
     pylab.plot(time, prey, '-g', label='prey')
@@ -54,8 +89,9 @@ def main(args):
     pylab.title('Predator and Prey Dynamics')
     figure1.savefig('results/fig1.png')
 
-
-    """ Plotting the evolution of the rabit and wolves populations"""
+    """ Plotting the phase curve with the vector field"""
+    plot_phase_curve(get_growth_rates, alpha, beta, gamma, delta, time)
+    
 
 
 if __name__=='__main__':
